2e5f4e007617a44c3988152870919f664e54a135
Irfan Yilmaz <iy.usa@outlook.com>
2023-12-10 18:05:15 -0500

    implement functionality and styling for drop down menu

---
+++ src/dropDown.js
@@ -0,0 +1,29 @@
+
+const menuInnerHTML = `<div id="button-div">
+<button>Cart</button>
+<button>Saved</button>
+<button>Settings</button>
+</div>`;
+
+function createDropDown() {
+    const dropDownDiv = document.getElementById("drop-down-div");
+    const menu = document.createElement("div");
+    menu.setAttribute("id", "drop-down-menu");
+    menu.innerHTML = menuInnerHTML;
+    dropDownDiv.appendChild(menu);
+}
+
+function toggleDropDown() {
+    
+    if(document.getElementById("drop-down-menu").style.display === "none")
+    {
+        document.getElementById("drop-down-menu").style.display = "inline";
+    }
+    else {
+        document.getElementById("drop-down-menu").style.display = "none";
+    }
+
+};
+
+
+export {createDropDown, toggleDropDown}
--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
 import './style.css';
+import {createDropDown, toggleDropDown} from './dropDown';
 
+
+
+const dropDownButton = document.getElementById("drop-down-button");
+createDropDown();
+
+dropDownButton.addEventListener("click", toggleDropDown);
